e7aacf782e9c2adbacf64522b05d842a3354125a
Emmanuel Santiago <emmanuel.santiago6@upr.edu>
2018-09-16 20:25:18 -0400

    Payment formula added.

--- src/Position.java
+++ src/Position.java
@@ -66,10 +66,15 @@ public class Position {
 	 * @return 
 	 */
 	public double getYearlySalary() {
+		double totalYearly;
+		if (jobType == true){totalYearly = this.hSalary*40*4.5*12;
+		return totalYearly;
+		}
+		else{totalYearly = this.hSalary*20*4.5*12;
+		return totalYearly;
+		}
 		
-		//Add Code Here [You have to use If/Else to get graded]
 		
-		return 0; //Temporal Return
 		
 	}
 }
